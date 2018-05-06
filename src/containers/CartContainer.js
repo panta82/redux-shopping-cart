e1c3c9ef72e589a8b1b8a2f29fbbcf3572752609
Ivan Pantic <ivanpantic82@gmail.com>
2018-05-06 14:54:18 +0200

    Dehipsterized

--- src/containers/CartContainer.js
+++ src/containers/CartContainer.js
@@ -1,34 +1,34 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { checkout } from '../actions'
-import { getTotal, getCartProducts } from '../reducers'
-import Cart from '../components/Cart'
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {checkout} from '../actions';
+import {getCartProducts, getTotal} from '../reducers';
+import Cart from '../components/Cart';
 
-const CartContainer = ({ products, total, checkout }) => (
-  <Cart
-    products={products}
-    total={total}
-    onCheckoutClicked={() => checkout(products)} />
-)
+const CartContainer = ({products, total, checkout}) => (
+	<Cart
+		products={products}
+		total={total}
+		onCheckoutClicked={() => checkout(products)}/>
+);
 
 CartContainer.propTypes = {
-  products: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    quantity: PropTypes.number.isRequired
-  })).isRequired,
-  total: PropTypes.string,
-  checkout: PropTypes.func.isRequired
-}
+	products: PropTypes.arrayOf(PropTypes.shape({
+		id: PropTypes.number.isRequired,
+		title: PropTypes.string.isRequired,
+		price: PropTypes.number.isRequired,
+		quantity: PropTypes.number.isRequired
+	})).isRequired,
+	total: PropTypes.string,
+	checkout: PropTypes.func.isRequired
+};
 
 const mapStateToProps = (state) => ({
-  products: getCartProducts(state),
-  total: getTotal(state)
-})
+	products: getCartProducts(state),
+	total: getTotal(state)
+});
 
 export default connect(
-  mapStateToProps,
-  { checkout }
-)(CartContainer)
+	mapStateToProps,
+	{checkout}
+)(CartContainer);
--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import { createLogger } from 'redux-logger'
-import thunk from 'redux-thunk'
-import reducer from './reducers'
-import { getAllProducts } from './actions'
-import App from './containers/App'
+import React from 'react';
+import {render} from 'react-dom';
+import {applyMiddleware, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {createLogger} from 'redux-logger';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+import {getAllProducts} from './actions';
+import App from './containers/App';
 
-const middleware = [ thunk ];
+const middleware = [thunk];
 if (process.env.NODE_ENV !== 'production') {
-  middleware.push(createLogger());
+	middleware.push(createLogger());
 }
 
 const store = createStore(
-  reducer,
-  applyMiddleware(...middleware)
-)
+	reducer,
+	applyMiddleware(...middleware)
+);
 
-store.dispatch(getAllProducts())
+store.dispatch(getAllProducts());
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+	<Provider store={store}>
+		<App/>
+	</Provider>,
+	document.getElementById('root')
+);
